7f67c3518f1610cc974d28ad8650c4fd3275adbb
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-12-13 20:59:36 +0600

    test file created

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,7 @@
+import LoginPage from "../../PageObjects/LoginPage.js";
+
+describe("TS:Checkout and contact form submission", () => {
+  it("TC:001", () => {
+    cy.visit("https://automationexercise.com/login");
+  });
+});
