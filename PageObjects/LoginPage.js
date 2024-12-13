2621481a2ccfdbd1d6ddfe59907a588df1071e5b
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-12-13 21:29:18 +0600

    sign up elements added

--- PageObjects/LoginPage.js
+++ PageObjects/LoginPage.js
@@ -3,6 +3,19 @@ import BasePage from "./BasePage.js";
 class LoginPage extends BasePage {
   constructor() {
     super(`//div[@class="login-form"]//h2`, "Login Page");
+    this.signUpUserNameField = `//input[@data-qa="signup-name"]`;
+    this.signUpEmailField = `//input[@data-qa="signup-email"]`;
+    this.signUpButton = `//button[@data-qa="signup-button"]`;
+  }
+  setSignUpName(name) {
+    cy.xpath(this.signUpUserNameField).type(name);
+  }
+  setSignUpEmail(email) {
+    cy.xpath(this.signUpEmailField).type(email);
+  }
+
+  clickSignUpButton() {
+    cy.xpath(this.signUpButton).click();
   }
 }
 
