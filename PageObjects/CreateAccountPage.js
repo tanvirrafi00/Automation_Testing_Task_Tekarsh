240289660dc893ea4a32904e00039bcafba421b0
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-12-13 23:36:49 +0600

    element added

---
+++ PageObjects/CreateAccountPage.js
@@ -0,0 +1,52 @@
+import BasePage from "./BasePage.js";
+
+class CreateAccountPage extends BasePage {
+  constructor() {
+    super(`//*[text()="Enter Account Information"]`, "Create Account Page");
+    this.mrTitle = `//div[@id="uniform-id_gender1"]`;
+    this.mrsTitle = `//div[@id="uniform-id_gender2"]`;
+    this.passwordField = `//input[@id="password"]`;
+    this.birthDay = `//select[@id="days"]`;
+    this.birthMonth = `//select[@id="months"]`;
+    this.birthYear = `//select[@id="years"]`;
+    this.newsLetterCheckBox = `//input[@id="newsletter"]`;
+    this.specialOfferCheckBox = `//input[@id="optin"]`;
+    this.firstNameField = `//input[@id="first_name"]`;
+    this.lastNameField = `//input[@id="last_name"]`;
+    this.companyField = `//input[@id="company"]`;
+    this.address1Field = `//input[@id="address1"]`;
+    this.address2Field = `//input[@id="address2"]`;
+    this.country = `//select[@id="country"]`;
+    this.stateField = `//input[@id="state"]`;
+    this.cityField = `//input[@id="city"]`;
+    this.zipcodeField = `//input[@id="zipcode"]`;
+    this.phoneNumberField = `//input[@id="mobile_number"]`;
+    this.createAccountButton = `//button[@data-qa="create-account"]`;
+  }
+
+  fillRegistrationForm(data) {
+    cy.xpath(this.mrTitle).click();
+    cy.xpath(this.passwordField).type(data.password);
+    cy.xpath(this.birthDay).select(data.birthDay);
+    cy.xpath(this.birthMonth).select(data.birthMonth);
+    cy.xpath(this.birthYear).select(data.birthYear);
+    cy.xpath(this.newsLetterCheckBox).check();
+    cy.xpath(this.specialOfferCheckBox).check();
+    cy.xpath(this.firstNameField).type(data.firstName);
+    cy.xpath(this.lastNameField).type(data.lastName);
+    cy.xpath(this.companyField).type(data.companyName);
+    cy.xpath(this.address1Field).type(data.addressLine1);
+    cy.xpath(this.address2Field).type(data.addressLine2);
+    cy.xpath(this.country).select(data.country);
+    cy.xpath(this.stateField).type(data.state);
+    cy.xpath(this.cityField).type(data.city);
+    cy.xpath(this.zipcodeField).type(data.zipCode);
+    cy.xpath(this.phoneNumberField).type(data.mobileNumber);
+  }
+
+  clickCreateAccountButton() {
+    cy.xpath(this.createAccountButton).click();
+  }
+}
+
+export default new CreateAccountPage();
