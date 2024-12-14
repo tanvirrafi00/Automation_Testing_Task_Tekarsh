302a1e947d453acf8be00cab5063fd2a2fa42e97
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-12-14 12:46:17 +0600

    registration process added

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,7 +1,30 @@
+import CreateAccountPage from "../../PageObjects/CreateAccountPage.js";
 import LoginPage from "../../PageObjects/LoginPage.js";
 
 describe("TS:Checkout and contact form submission", () => {
   it("TC:001", () => {
     cy.visit("https://automationexercise.com/login");
+
+    cy.fixture("registrationData.json").then((data) => {
+      cy.log(data.name);
+      LoginPage.setSignUpName(data.name);
+      LoginPage.setSignUpEmail(data.email);
+    });
+
+    LoginPage.clickSignUpButton();
+
+    cy.fixture("registrationData.json").then((data) => {
+      CreateAccountPage.fillRegistrationForm(data);
+    });
+
+    CreateAccountPage.clickCreateAccountButton();
+    cy.log("hello");
+
+    cy.fixture("messageData.json").then((data) => {
+      cy.log(data.accountCreatedMessage);
+      CreateAccountPage.checkAccountCreationMessage(data.accountCreatedMessage);
+    });
+
+    CreateAccountPage.clickContinueButton();
   });
 });
